6527eb6aa40b8200712dce38c6946c120f4b7ff7
Abdelrahman Omran <me@omranic.com>
2017-06-04 15:51:21 +0200

    Fix wrong slug generation method order

--- src/Models/Attribute.php
+++ src/Models/Attribute.php
@@ -96,7 +96,8 @@ class Attribute extends Model implements Sortable
     protected $rules = [];
 
     /**
-     * Whether the model should throw a ValidationException if it fails validation.
+     * Whether the model should throw a
+     * ValidationException if it fails validation.
      *
      * @var bool
      */
@@ -129,12 +130,12 @@ class Attribute extends Model implements Sortable
 
         if (isset(static::$dispatcher)) {
             // Early auto generate slugs before validation
-            static::$dispatcher->listen('eloquent.validating: '.static::class, function ($model, $event) {
+            static::$dispatcher->listen('eloquent.validating: '.static::class, function (self $model) {
                 if (! $model->slug) {
                     if ($model->exists) {
-                        $model->generateSlugOnCreate();
-                    } else {
                         $model->generateSlugOnUpdate();
+                    } else {
+                        $model->generateSlugOnCreate();
                     }
                 }
             });
