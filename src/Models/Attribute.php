f4db61c6479ce0582f4ea440a46a78c45e9e3a67
Abdelrahman Omran <me@omranic.com>
2017-07-22 02:05:00 +0200

    Tweak validation rules

--- src/Models/Attribute.php
+++ src/Models/Attribute.php
@@ -137,11 +137,11 @@ class Attribute extends Model implements Sortable
             'name' => 'required|string|max:150',
             'description' => 'nullable|string|max:10000',
             'slug' => 'required|alpha_dash|max:150|unique:'.config('rinvex.attributable.tables.attributes').',slug',
-            'sort_order' => 'integer|max:10000000',
+            'sort_order' => 'sometimes|required|integer|max:10000000',
             'group' => 'nullable|string|max:150',
             'type' => 'required|string|max:150',
-            'is_required' => 'boolean',
-            'is_collection' => 'boolean',
+            'is_required' => 'sometimes|required|boolean',
+            'is_collection' => 'sometimes|required|boolean',
             'default' => 'nullable|string|max:10000',
         ]);
     }
