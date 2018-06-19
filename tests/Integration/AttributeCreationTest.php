bdcf81308a665dc84041b0c4904815da04c0c450
Israel Ortuño <ai.ortuno@gmail.com>
2018-06-19 11:16:01 +0200

    Slug testing

--- tests/Integration/AttributeCreationTest.php
+++ tests/Integration/AttributeCreationTest.php
@@ -2,16 +2,30 @@
 
 class AttributeCreationTest extends \TestCase
 {
-    public function testAttributeCreation()
+    public function testBasicAttributeCreation()
     {
-        $attribute = app('rinvex.attributes.attribute')->create([
-            'slug' => 'count',
-            'type' => 'integer',
-            'name' => 'Count',
-            'entities' => ['User'],
-        ]);
+        $attribute = $this->createAttribute();
 
         $this->assertDatabaseHas('attributes', ['slug' => 'count', 'type' => 'integer']);
         $this->assertDatabaseHas('attribute_entity', ['attribute_id' => $attribute->id, 'entity_type' => 'User']);
+    }
+
+    public function testAttributeSlugShouldBeSnakeCase()
+    {
+        $attribute = $this->createAttribute(['name' => 'foo-bar']);
+
+        $this->assertEquals('foo_bar', $attribute->slug);
+        $this->assertDatabaseHas('attributes', ['slug' => 'foo_bar', 'type' => 'integer']);
+    }
+
+    // TODO: Add testing for slug check when slug is provided in the creation attributes
+
+    protected function createAttribute($attributes = [])
+    {
+        return app('rinvex.attributes.attribute')->create(array_merge([
+            'type'     => 'integer',
+            'name'     => 'Count',
+            'entities' => ['User'],
+        ], $attributes));
     }
 }
