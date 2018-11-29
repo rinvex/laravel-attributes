4abf1a7b051d888d34778b2be11272ba0c9b9f68
Babichev Maxim <dev@babichev.net>
2018-11-29 17:58:44 +0300

    Update 2017_01_19_040614_create_attributes_table.php

--- database/migrations/2017_01_19_040614_create_attributes_table.php
+++ database/migrations/2017_01_19_040614_create_attributes_table.php
@@ -51,8 +51,14 @@ class CreateAttributesTable extends Migration
      */
     protected function jsonable(): string
     {
-        return DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) === 'mysql'
-               && version_compare(DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION), '5.7.8', 'ge')
-            ? 'json' : 'text';
+        $driverName = DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME);
+        $dbVersion = DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
+        $isOldVersion = version_compare($dbVersion, '5.7.8', 'lt');
+        
+        if ($driverName === 'mysql' && $isOldVersion) {
+            return 'text';   
+        }
+        
+        return 'json';
     }
 }
