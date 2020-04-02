f57748eec828b4b9c199c136baee1f79c2d27437
Flakerim Ismani <flakerimi@gmail.com>
2020-04-02 06:20:25 +0200

    Update 2020_01_01_000005_create_attribute_float_values_table.php

--- database/migrations/2020_01_01_000005_create_attribute_float_values_table.php
+++ database/migrations/2020_01_01_000005_create_attribute_float_values_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAttributeFloadValuesTable extends Migration
+class CreateAttributeFloatValuesTable extends Migration
 {
     /**
      * Run the migrations.
