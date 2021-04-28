41a4feb812769ae96142916c3976a6087aea3e88
Saulius Kazokas <saulens22@gmail.com>
2021-04-28 11:30:07 +0300

    Add factory to model

--- tests/Stubs/User.php
+++ tests/Stubs/User.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace Rinvex\Attributes\Tests\Stubs;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Rinvex\Attributes\Tests\Factories\UserFactory;
 use Rinvex\Attributes\Traits\Attributable;
 
 class User extends Model
 {
-    use Attributable;
+    use Attributable, HasFactory;
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return UserFactory::new();
+    }
 }
